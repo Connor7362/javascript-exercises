6c289ad4825298eca933c01bafc1b4f324fd0888
Connor7362 <Connorebauer@gmail.com>
2025-07-12 21:30:27 -0500

    Finished 6-11 exercises

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let rev = '';
+    for(let i = word.length -1 ; i >= 0; i--){
+         rev += word[i];
+    }
+    return rev;
 
 };
 
