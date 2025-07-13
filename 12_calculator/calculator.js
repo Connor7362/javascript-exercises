6b276ff31909c00e56563dd730b1eba377c3fc6e
Connor7362 <Connorebauer@gmail.com>
2025-07-13 15:15:48 -0500

    Finished the Javascript exercises

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(one , two) {
+	return one + two;
 };
 
-const subtract = function() {
-	
+const subtract = function(one, two) {
+	return one - two;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function(one , two) {
+	return Math.pow(one, two);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
